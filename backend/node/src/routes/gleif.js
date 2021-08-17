61730c0d3a61fc603b778a5524045a2fec2a0e52
Hans de Rooij <HansDeRooij@Hotmail.com>
2021-08-17 17:16:22 +0000

    Implemented LEI validity check

--- backend/node/src/routes/gleif.js
+++ backend/node/src/routes/gleif.js
@@ -27,6 +27,27 @@ import getHttpRespPromise from '../ah_rpr_http.js';
 
 const router = express.Router();
 
+function isKeyValid(sKey) {
+   let m = 0, charCode;
+
+   for(let i = 0; i < sKey.length; i++) {
+      charCode = sKey.charCodeAt(i);
+   
+      if(charCode >= 48 && charCode <= 57) {
+         m = (m * 10 + charCode - 48) % 97 
+      }
+      else if(charCode >= 65 && charCode <= 90) {
+         m = (m * 100 + charCode - 55) % 97 
+      }
+      else {
+         console.log(`Unexpected character at ${i}`);
+         return false;
+      }
+   }
+
+   return m === 1;
+}
+
 router.get('/', (req, resp) => {
    resp.json({api: ahEndpoints[ahProviderCodes.gleif].provider , key: ahKeys[ahKeyCodes.lei]})
 });
@@ -35,8 +56,8 @@ router.get(`/${ahKeys[ahKeyCodes.lei]}/:key`, (req, resp) => {
 
    const lei = req.params.key;
 
-   if(!lei) {
-      const ahErr = new ApiHubErr(ahErrCodes.invalidParameter, 'LEI specified is not valid');
+   if(!isKeyValid(lei)) {
+      const ahErr = new ApiHubErr(ahErrCodes.invalidParameter, 'LEI submitted is not valid');
 
       resp.status(ahErr.apiHubErr.http.status).json(ahErr); return;
    }
