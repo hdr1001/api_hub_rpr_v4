6c08f204cd061c20081670e374a1950b969a62a9
hdr1001 <hansderooij@hotmail.com>
2021-10-24 13:02:01 +0200

    Added state and content to reflect state

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,47 +1,38 @@
 import React from 'react';
 import { styled } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import UploadFileOutlinedIcon from '@mui/icons-material/UploadFileOutlined';
-import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import AhAppBar from './AhAppBar';
 
-export default function App(props) {
-   const Offset = styled('div')(({ theme }) => theme.mixins.toolbar);
+export default class App extends React.Component {
+   constructor(props) {
+      super(props);
 
-   return (
-      <>
-         <AppBar>
-            <Toolbar>
-               <Typography sx={{ flexGrow: 1 }}>API Hub (v4)</Typography>
-               <input
-                  type='file'
-                  accept='application/json, .json'
-                  multiple
-                  style={{ display: 'none' }}
-                  id='btn-file-inp'
-                  onChange={e => alert(Array.from(e.target.files).map(file => file.name).join(', '))}
-               />
-               <label htmlFor='btn-file-inp'>
-                  <IconButton
-                     color='inherit'
-                     component='span'
-                  >
-                     <UploadFileOutlinedIcon />
-                  </IconButton>
-               </label>
-               <IconButton
-                  color='inherit'
-                  onClick={() => {alert('Not yet implemented, please try again later')}}
-               >
-                  <SettingsOutlinedIcon />
-               </IconButton>
-            </Toolbar>
-         </AppBar>
+      this.state = {
+         arrFiles: []
+      };
 
-         <Offset />
-         <Typography>Content here</Typography>
-      </>
-   )
+      this.handleFileInp = this.handleFileInp.bind(this);
+   }
+
+   handleFileInp = e => {
+      this.setState(
+         { arrFiles: this.state.arrFiles.concat(Array.from(e.target.files).map(file => file.name)) }
+      )
+   }
+
+   render() {
+      const Offset = styled('div')(({ theme }) => theme.mixins.toolbar);
+
+      return (
+         <>
+            <AhAppBar handleFileInp={this.handleFileInp} />
+
+            <Offset />
+            <table><tbody>
+               {
+                  this.state.arrFiles.map((file, idx) => <tr key={idx}><td>{file}</td></tr>)
+               }
+            </tbody></table>
+         </>
+      )
+   }
 }
