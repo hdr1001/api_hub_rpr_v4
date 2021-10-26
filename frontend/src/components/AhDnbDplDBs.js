844c746327538df0f2fb3c2269e880be62fbdf28
hdr1001 <hansderooij@hotmail.com>
2021-10-26 20:36:23 +0200

    Added fileReader & JSON.parse

---
+++ frontend/src/components/AhDnbDplDBs.js
@@ -0,0 +1,63 @@
+// *********************************************************************
+//
+// API Hub request, persist and respond UI D&B Direct+ component
+// JavaScript code file: AhDnbDplDBs.js
+//
+// Copyright 2021 Hans de Rooij
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, 
+// software distributed under the License is distributed on an 
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+// either express or implied. See the License for the specific 
+// language governing permissions and limitations under the 
+// License.
+//
+// *********************************************************************
+
+import React, { useState, useEffect } from 'react';
+import Card from '@mui/material/Card';
+
+export default function DnbDplDBs(props) {
+   const [oDbData, setObjDbData] = useState(null);
+   const [errMsg, setErrMsg] = useState('');
+
+   useEffect(() => {
+      const fileReader = new FileReader;
+   
+      fileReader.onload = evnt => {
+         try {
+            setObjDbData(JSON.parse(evnt.target.result));
+         }
+         catch(err) {
+            setErrMsg(err.message ? err.message : `Error parsing file ${props.file.name}`);
+         }
+      }
+   
+      fileReader.onerror = err => {
+         setErrMsg(err.message ? err.message : `Error reading file ${props.file.name}`)
+      };
+   
+      fileReader.readAsText(props.file, 'utf-8');
+   }, []);
+
+   return (
+      <>
+         {errMsg
+            ?
+               <Card sx={{ px: 0.5 }}>
+                  {errMsg}
+               </Card>
+            :
+               <Card sx={{ px: 0.5 }}>
+                  {oDbData && JSON.stringify(oDbData, null, 3)}
+               </Card>
+         }
+      </>
+   );
+}
--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,3 +1,25 @@
+// *********************************************************************
+//
+// API Hub request, persist and respond UI main app
+// JavaScript code file: App.js
+//
+// Copyright 2021 Hans de Rooij
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, 
+// software distributed under the License is distributed on an 
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+// either express or implied. See the License for the specific 
+// language governing permissions and limitations under the 
+// License.
+//
+// *********************************************************************
+
 import React from 'react';
 import { styled } from '@mui/material/styles';
 import AhAppBar from './AhAppBar';
@@ -16,7 +38,7 @@ export default class App extends React.Component {
 
    handleFileInp = e => {
       this.setState(
-         { arrFiles: this.state.arrFiles.concat(Array.from(e.target.files).map(file => file.name)) }
+         { arrFiles: this.state.arrFiles.concat(Array.from(e.target.files)) }
       )
    }
 
