2298f2215c0f59050fd7c2a50b0dba78dca5679b
Hans de Rooij <hansderooij@hotmail.com>
2021-11-24 19:36:34 +0100

    Added D&B standard rating section

--- frontend/src/components/dnb/DplFinStrength.js
+++ frontend/src/components/dnb/DplFinStrength.js
@@ -22,11 +22,89 @@
 
 import React from 'react';
 import { B2BDataTable, B2BDataTableRow, bIsEmptyObj } from '../AhUtils';
+import { oCurrOpts } from '../style'
 
 //Data block Hierarchies & Connections component
 export default function DbFinStrength(props) {
    if(!(props.content && props.content.organization)) {
       return null
+   }
+
+   function Rating(props) {
+      if(!(props.content && props.content.organization && 
+            !bIsEmptyObj(props.content.organization.dnbAssessment) &&
+            !bIsEmptyObj(props.content.organization.dnbAssessment.standardRating))) {
+         return null
+      }
+
+      //Standard rating properties
+      const { rating,
+         financialStrength,
+         riskSegment,
+         scoreDate,
+         ratingReason,
+         ratingOverrideReasons,
+      } = props.content.organization.dnbAssessment.standardRating;
+
+      //Credit limit recommendation properties
+      const { maximumRecommendedLimit,
+         assessmentDate
+      } = props.content.organization.dnbAssessment.creditLimitRecommendation || {};
+
+      let sMaxCreditLimit = '';
+
+      if(maximumRecommendedLimit && maximumRecommendedLimit.value && 
+            (typeof maximumRecommendedLimit.value === 'number')) {
+
+         sMaxCreditLimit = maximumRecommendedLimit.value.toString();
+
+         if(maximumRecommendedLimit.currency) {
+            let oCurrency = { ...oCurrOpts, currency: maximumRecommendedLimit.currency }
+
+            const intlNumFormat = new Intl.NumberFormat('en-us', oCurrency);
+   
+            sMaxCreditLimit = intlNumFormat.format(maximumRecommendedLimit.value)
+         }
+      }
+
+      return (
+         <B2BDataTable caption='D&amp;B standard rating'>
+            {!(financialStrength && riskSegment) && rating
+               ?
+                  <B2BDataTableRow label='D&amp;B Rating' content={rating} />
+               :
+                  <>
+                     {financialStrength &&
+                        <B2BDataTableRow label='Financial strength' content={financialStrength} />
+                     }
+                     {riskSegment &&
+                        <B2BDataTableRow label='Risk segment' content={riskSegment} />
+                     }
+                  </>
+            }
+            {(rating || financialStrength || riskSegment) && scoreDate &&
+               <B2BDataTableRow label='Score date' content={scoreDate} />
+            }
+            {sMaxCreditLimit.length > 0 &&
+               <B2BDataTableRow label='Max rec credit limit' content={sMaxCreditLimit} />
+            }
+            {assessmentDate &&
+               <B2BDataTableRow label='Credit limit assmnt date' content={assessmentDate} />
+            }
+            {ratingReason && ratingReason.length > 0 &&
+               <B2BDataTableRow
+                  label='Rating reason(s)'
+                  content={ratingReason.map(reason => reason.description)}
+               />
+            }
+            {ratingOverrideReasons && ratingOverrideReasons.length > 0 &&
+               <B2BDataTableRow
+                  label='Override reason(s)'
+                  content={ratingOverrideReasons.map(override => override.description)}
+               />
+            }
+         </B2BDataTable>
+      );
    }
 
    function FailureScore(props) {
@@ -71,6 +149,7 @@ export default function DbFinStrength(props) {
 
    return (
       <>
+         <Rating content={props.content} />
          <FailureScore content={props.content} />
       </>
    );
